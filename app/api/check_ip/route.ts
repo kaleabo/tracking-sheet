110a9cb4189b794fbb4fd26e2646b54aa27750be
Kaleab Selamawi <selamawi.gd@gmail.com>
2024-06-29 05:52:11 -0700

    update route.ts

--- app/api/check_ip/route.ts
+++ app/api/check_ip/route.ts
@@ -3,20 +3,16 @@ import { v4 as uuidv4 } from "uuid";
 
 export async function GET(req: NextRequest) {
   // List of authorized IP addresses
-  const authorizedIps = ["192.168.1.1", "203.0.113.5"];
+  const authorizedIps = ["192.168.1.1", "203.0.113.5", "196.191.112.120"];
 
   // Get the user's IP address
-  const userIpx = req.headers.get("X-Forwarded-For") || req.ip;
-  const userIp = "192.168.1.1";
+  const userIp: any = req.headers.get("X-Forwarded-For") || req.ip;
 
   // Check if the IP address is authorized
   if (authorizedIps.includes(userIp)) {
     // Generate a unique token
     const token = uuidv4();
-    return Response.json(
-      { userIp: userIpx, authorized: true, token: token },
-      { status: 200 }
-    );
+    return Response.json({ authorized: true, token: token }, { status: 200 });
   } else {
     return Response.json({ authorized: false }, { status: 403 });
   }
